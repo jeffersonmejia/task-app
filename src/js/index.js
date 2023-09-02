e14a3e33099f99269e84ead9fa60ff87a0362d45
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-09-02 17:00:05 -0500

    add new features

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,192 @@
 "use strict";
-const d = document, $addTaskForm = d.querySelector('.add-task-form'), $taskList = d.querySelector('.task-list');
-function handleAddTask(buttonTask) {
-    const buttonContent = buttonTask === null || buttonTask === void 0 ? void 0 : buttonTask.textContent, isAdd = buttonContent === null || buttonContent === void 0 ? void 0 : buttonContent.includes('Add');
-    if (isAdd) {
-        $taskList === null || $taskList === void 0 ? void 0 : $taskList.classList.add('hidden');
-        buttonTask.textContent = 'Home';
-        $addTaskForm === null || $addTaskForm === void 0 ? void 0 : $addTaskForm.classList.remove('hidden');
+const d = document, $addTaskForm = d.querySelector('.add-task-form'), $tasksList = d.querySelector('.task-list'), $usernameContent = d.querySelector('.welcome-user-title h2'), $taskTemplate = d.getElementById('task-template'), $sectionButton = d.querySelector('.add-task-button'), $addTaskSubmit = d.querySelector('#add-task-submit'), $noTask = d.querySelector('.not-task-text');
+function handleAddTask() {
+    let buttonContent, isAdd = false;
+    if ($sectionButton) {
+        buttonContent = ($sectionButton === null || $sectionButton === void 0 ? void 0 : $sectionButton.textContent) || '';
+        isAdd = (buttonContent === null || buttonContent === void 0 ? void 0 : buttonContent.includes('Add')) || false;
+        if (isAdd) {
+            $tasksList === null || $tasksList === void 0 ? void 0 : $tasksList.classList.add('hidden');
+            $sectionButton.textContent = 'Home';
+            $addTaskForm === null || $addTaskForm === void 0 ? void 0 : $addTaskForm.classList.remove('hidden');
+        }
+        else {
+            $sectionButton.textContent = 'Add new task';
+            $addTaskForm === null || $addTaskForm === void 0 ? void 0 : $addTaskForm.classList.add('hidden');
+            $tasksList === null || $tasksList === void 0 ? void 0 : $tasksList.classList.remove('hidden');
+        }
+        $sectionButton.textContent = isAdd ? 'Home' : 'Add new task';
+    }
+}
+function getUser() {
+    const isUsername = localStorage.getItem('username');
+    if ($usernameContent) {
+        if (!isUsername) {
+            $usernameContent.textContent = 'Type your name...';
+            $usernameContent.contentEditable = 'true';
+            $usernameContent.focus();
+        }
+        else {
+            let username = localStorage.getItem('username') || 'Unknown';
+            if (username === null || username === void 0 ? void 0 : username.includes('Type')) {
+                $usernameContent.textContent = 'Type your name...';
+                $usernameContent.contentEditable = 'true';
+                $usernameContent.focus();
+            }
+            else {
+                $usernameContent.textContent = `Bienvenido, ${username}`;
+            }
+        }
+    }
+}
+function getTaskId() {
+    const tasks = localStorage.getItem('tasks') || '';
+    let array = [], id = 0;
+    if (tasks.length > 0) {
+        array = JSON.parse(tasks);
+    }
+    if (array.length > 0) {
+        id = array.length;
+    }
+    return id;
+}
+function getNewTasksValues() {
+    const inputsHTML = $addTaskForm === null || $addTaskForm === void 0 ? void 0 : $addTaskForm.querySelectorAll('.input-group input');
+    let data = {}, inputs = [], newData = [], id = getTaskId();
+    if (inputsHTML) {
+        inputs = Array.from(inputsHTML);
+    }
+    inputs === null || inputs === void 0 ? void 0 : inputs.forEach((input) => {
+        data = Object.assign(Object.assign({}, data), { [input.id]: input.value });
+    });
+    data = Object.assign(Object.assign({}, data), { id });
+    newData.push(data);
+    return newData;
+}
+function getTasksValues() {
+    const old = localStorage.getItem('tasks');
+    let _new = getNewTasksValues(), oldArray = [], newArray = [], joined = [];
+    if (old && old.length > 0) {
+        oldArray = JSON.parse(old);
+        joined = oldArray;
+    }
+    if (_new && _new.length > 0) {
+        joined = [...joined, ..._new];
+    }
+    return joined;
+}
+function handleNewTaks() {
+    const tasks = getTasksValues(), json = JSON.stringify(tasks);
+    console.log(json);
+    localStorage.setItem('tasks', json);
+    $addTaskForm === null || $addTaskForm === void 0 ? void 0 : $addTaskForm.reset();
+    if (!($noTask === null || $noTask === void 0 ? void 0 : $noTask.classList.contains('hidden'))) {
+        $noTask === null || $noTask === void 0 ? void 0 : $noTask.classList.add('hidden');
+    }
+    loadSavedTasks();
+}
+function getSavedTasks() {
+    const tasks = localStorage.getItem('tasks') || '';
+    let json = [];
+    if (tasks) {
+        json = JSON.parse(tasks);
+    }
+    return json;
+}
+function loadSavedTasks() {
+    const tasks = getSavedTasks(), $fragment = d.createDocumentFragment();
+    let template = null;
+    if ($taskTemplate) {
+        template = $taskTemplate.content;
+    }
+    if (tasks.length > 0) {
+        tasks.forEach((task, index) => {
+            const $clone = template === null || template === void 0 ? void 0 : template.cloneNode(true);
+            if ($clone) {
+                const name = $clone.querySelector('h3'), description = $clone.querySelector('p'), date = $clone.querySelector('.task-date-item'), time = $clone.querySelector('.task-time-item'), deleteButton = $clone.querySelector('.delete-task'), editButton = $clone.querySelector('.edit-task');
+                if (name) {
+                    name.textContent = task.name || 'No named';
+                }
+                if (description) {
+                    description.textContent = task.description || 'No description';
+                }
+                if (date) {
+                    date.textContent = task.date || '00/00/00';
+                }
+                if (time) {
+                    time.textContent = task.time || '--:--';
+                }
+                if (deleteButton) {
+                    deleteButton.setAttribute('data-id', `${task.id}`);
+                }
+                if (editButton) {
+                    editButton.setAttribute('data-id', `${task.id}`);
+                }
+            }
+            $fragment.appendChild($clone);
+        });
+        $tasksList === null || $tasksList === void 0 ? void 0 : $tasksList.appendChild($fragment);
     }
     else {
-        buttonTask.textContent = 'Add new task';
-        $addTaskForm === null || $addTaskForm === void 0 ? void 0 : $addTaskForm.classList.add('hidden');
-        $taskList === null || $taskList === void 0 ? void 0 : $taskList.classList.remove('hidden');
+        $noTask === null || $noTask === void 0 ? void 0 : $noTask.classList.remove('hidden');
     }
-    buttonTask.textContent = isAdd ? 'Home' : 'Add new task';
 }
+function editTask(id) {
+    handleAddTask();
+    if ($addTaskForm) {
+        const name = $addTaskForm.querySelector('#name'), description = $addTaskForm.querySelector('#description'), date = $addTaskForm.querySelector('#date'), time = $addTaskForm.querySelector('#time');
+        const tasks = getSavedTasks();
+        const DEFAULT_TASK = {
+            id: -1,
+            date: '',
+            description: '',
+            name: '',
+            time: '',
+        };
+        const task = tasks.find((task) => task.id === id) || DEFAULT_TASK;
+        if (task.id !== -1) {
+            console.log(task);
+            if (name) {
+                name.value = task.name;
+            }
+            if (description) {
+                description.value = task.description;
+            }
+            if (date) {
+                date.value = task.date;
+            }
+            if (time) {
+                time.value = task.time;
+            }
+        }
+    }
+    if ($addTaskSubmit) {
+        $addTaskSubmit.value = 'Update';
+        $addTaskSubmit.setAttribute('data-update-id', `${id}`);
+    }
+}
+d.addEventListener('DOMContentLoaded', (e) => {
+    getUser();
+    loadSavedTasks();
+});
 d.addEventListener('click', (e) => {
     const target = e === null || e === void 0 ? void 0 : e.target;
     if (target.matches('.add-task-button')) {
-        handleAddTask(target);
+        handleAddTask();
+    }
+    if (target.matches('#add-task-submit')) {
+        e === null || e === void 0 ? void 0 : e.preventDefault();
+        handleNewTaks();
+    }
+    if (target.matches('.edit-task')) {
+        const id = parseInt(target.dataset.id || '0');
+        editTask(id);
+    }
+});
+d.addEventListener('keyup', (e) => {
+    const target = e === null || e === void 0 ? void 0 : e.target;
+    if (target.matches('.welcome-user-title h2')) {
+        const username = target.textContent || '';
+        localStorage.setItem('username', username);
     }
 });
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,27 +1,241 @@
 const d: Document = document,
 	$addTaskForm: HTMLFormElement | null = d.querySelector('.add-task-form'),
-	$taskList: HTMLElement | null = d.querySelector('.task-list')
+	$tasksList: HTMLElement | null = d.querySelector('.task-list'),
+	$usernameContent: HTMLElement | null = d.querySelector('.welcome-user-title h2'),
+	$taskTemplate: HTMLTemplateElement | null = d.getElementById(
+		'task-template'
+	) as HTMLTemplateElement,
+	$sectionButton: HTMLButtonElement | null = d.querySelector('.add-task-button'),
+	$addTaskSubmit: HTMLInputElement | null = d.querySelector('#add-task-submit'),
+	$noTask: HTMLElement | null = d.querySelector('.not-task-text')
 
-function handleAddTask(buttonTask: HTMLElement) {
-	const buttonContent = buttonTask?.textContent,
-		isAdd = buttonContent?.includes('Add')
+function handleAddTask(): void {
+	let buttonContent: string,
+		isAdd: boolean = false
+	if ($sectionButton) {
+		buttonContent = $sectionButton?.textContent || ''
+		isAdd = buttonContent?.includes('Add') || false
 
-	if (isAdd) {
-		$taskList?.classList.add('hidden')
-		buttonTask.textContent = 'Home'
-		$addTaskForm?.classList.remove('hidden')
-	} else {
-		buttonTask.textContent = 'Add new task'
-		$addTaskForm?.classList.add('hidden')
-		$taskList?.classList.remove('hidden')
+		if (isAdd) {
+			$tasksList?.classList.add('hidden')
+			$sectionButton.textContent = 'Home'
+			$addTaskForm?.classList.remove('hidden')
+		} else {
+			$sectionButton.textContent = 'Add new task'
+			$addTaskForm?.classList.add('hidden')
+			$tasksList?.classList.remove('hidden')
+		}
+
+		$sectionButton.textContent = isAdd ? 'Home' : 'Add new task'
+	}
+}
+
+function getUser(): void {
+	const isUsername = localStorage.getItem('username')
+
+	if ($usernameContent) {
+		if (!isUsername) {
+			$usernameContent.textContent = 'Type your name...'
+			$usernameContent.contentEditable = 'true'
+			$usernameContent.focus()
+		} else {
+			let username: string = localStorage.getItem('username') || 'Unknown'
+			if (username?.includes('Type')) {
+				$usernameContent.textContent = 'Type your name...'
+				$usernameContent.contentEditable = 'true'
+				$usernameContent.focus()
+			} else {
+				$usernameContent.textContent = `Bienvenido, ${username}`
+			}
+		}
+	}
+}
+
+function getTaskId(): number {
+	const tasks: string = localStorage.getItem('tasks') || ''
+	let array: object[] = [],
+		id = 0
+	if (tasks.length > 0) {
+		array = JSON.parse(tasks)
+	}
+	if (array.length > 0) {
+		id = array.length
+	}
+	return id
+}
+
+function getNewTasksValues(): object[] {
+	const inputsHTML: NodeList | undefined =
+		$addTaskForm?.querySelectorAll('.input-group input')
+	let data: object = {},
+		inputs: any[] = [],
+		newData: any[] = [],
+		id: number = getTaskId()
+
+	if (inputsHTML) {
+		inputs = Array.from(inputsHTML)
+	}
+	inputs?.forEach((input: HTMLInputElement) => {
+		data = { ...data, [input.id]: input.value }
+	})
+	data = { ...data, id }
+	newData.push(data)
+	return newData
+}
+
+function getTasksValues(): object[] {
+	const old: string | null = localStorage.getItem('tasks')
+	let _new: object[] = getNewTasksValues(),
+		oldArray: object[] = [],
+		newArray: object[] = [],
+		joined: object[] = []
+
+	if (old && old.length > 0) {
+		oldArray = JSON.parse(old)
+		joined = oldArray
+	}
+	if (_new && _new.length > 0) {
+		joined = [...joined, ..._new]
+	}
+	return joined
+}
+
+function handleNewTaks(): void {
+	const tasks: object[] = getTasksValues(),
+		json: string = JSON.stringify(tasks)
+	console.log(json)
+	localStorage.setItem('tasks', json)
+	$addTaskForm?.reset()
+	if (!$noTask?.classList.contains('hidden')) {
+		$noTask?.classList.add('hidden')
+	}
+	loadSavedTasks()
+}
+interface Task {
+	id: number
+	name: string
+	description: string
+	date: string
+	time: string
+}
+function getSavedTasks(): Task[] {
+	const tasks: string = localStorage.getItem('tasks') || ''
+	let json: Task[] = []
+	if (tasks) {
+		json = JSON.parse(tasks)
 	}
 
-	buttonTask.textContent = isAdd ? 'Home' : 'Add new task'
+	return json
 }
+
+function loadSavedTasks(): void {
+	const tasks: Task[] = getSavedTasks(),
+		$fragment: DocumentFragment = d.createDocumentFragment()
+
+	let template: DocumentFragment | null = null
+	if ($taskTemplate) {
+		template = $taskTemplate.content
+	}
+	if (tasks.length > 0) {
+		tasks.forEach((task: Task, index: number) => {
+			const $clone = template?.cloneNode(true) as HTMLElement
+			if ($clone) {
+				const name = $clone.querySelector('h3'),
+					description = $clone.querySelector('p'),
+					date = $clone.querySelector('.task-date-item'),
+					time = $clone.querySelector('.task-time-item'),
+					deleteButton = $clone.querySelector('.delete-task'),
+					editButton = $clone.querySelector('.edit-task')
+				if (name) {
+					name.textContent = task.name || 'No named'
+				}
+				if (description) {
+					description.textContent = task.description || 'No description'
+				}
+				if (date) {
+					date.textContent = task.date || '00/00/00'
+				}
+				if (time) {
+					time.textContent = task.time || '--:--'
+				}
+				if (deleteButton) {
+					deleteButton.setAttribute('data-id', `${task.id}`)
+				}
+				if (editButton) {
+					editButton.setAttribute('data-id', `${task.id}`)
+				}
+			}
+			$fragment.appendChild($clone)
+		})
+		$tasksList?.appendChild($fragment)
+	} else {
+		$noTask?.classList.remove('hidden')
+	}
+}
+
+function editTask(id: number): void {
+	handleAddTask()
+	if ($addTaskForm) {
+		const name: HTMLInputElement | null = $addTaskForm.querySelector('#name'),
+			description: HTMLInputElement | null = $addTaskForm.querySelector('#description'),
+			date: HTMLInputElement | null = $addTaskForm.querySelector('#date'),
+			time: HTMLInputElement | null = $addTaskForm.querySelector('#time')
+
+		const tasks: Task[] = getSavedTasks()
+		const DEFAULT_TASK: Task = {
+			id: -1,
+			date: '',
+			description: '',
+			name: '',
+			time: '',
+		}
+		const task: Task = tasks.find((task: Task) => task.id === id) || DEFAULT_TASK
+		if (task.id !== -1) {
+			console.log(task)
+			if (name) {
+				name.value = task.name
+			}
+			if (description) {
+				description.value = task.description
+			}
+			if (date) {
+				date.value = task.date
+			}
+			if (time) {
+				time.value = task.time
+			}
+		}
+	}
+	if ($addTaskSubmit) {
+		$addTaskSubmit.value = 'Update'
+		$addTaskSubmit.setAttribute('data-update-id', `${id}`)
+	}
+}
+
+d.addEventListener('DOMContentLoaded', (e: Event | null) => {
+	getUser()
+	loadSavedTasks()
+})
 
 d.addEventListener('click', (e: Event | null) => {
 	const target = e?.target as HTMLElement
 	if (target.matches('.add-task-button')) {
-		handleAddTask(target)
+		handleAddTask()
+	}
+	if (target.matches('#add-task-submit')) {
+		e?.preventDefault()
+		handleNewTaks()
+	}
+	if (target.matches('.edit-task')) {
+		const id: number = parseInt(target.dataset.id || '0')
+		editTask(id)
+	}
+})
+
+d.addEventListener('keyup', (e: Event | null) => {
+	const target = e?.target as HTMLElement
+	if (target.matches('.welcome-user-title h2')) {
+		const username: string = target.textContent || ''
+		localStorage.setItem('username', username)
 	}
 })
